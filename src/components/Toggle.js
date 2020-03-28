06cff165027fd76a44fbbdcb9454fb3f2443719f
Teresa Malave <dev.malave7@gmail.com>
2020-03-28 14:43:25 -0500

    starting to add onClick on Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -17,6 +17,8 @@ export default class Toggle extends Component {
             <h1>Box toggles b/w color if you click</h1>
             <div className='colorToggle'
             style={{backgroundColor: this.state.color}}
+            // Setting even property 1st to make it above and make sure it is calling the proper functionalty
+            onClick={this.toggleMe}
             >
 
             <p>Toggle me. I dare you</p>
